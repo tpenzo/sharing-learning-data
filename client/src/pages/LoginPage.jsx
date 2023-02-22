da213f33334ab49d08ffeefe792d6494ea424aca
vinhho3010 <anhvinh30102001@gmail.com>
2023-02-22 17:02:31 +0700

    complete login page interface

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -4,10 +4,11 @@ function LoginPage() {
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
   const [isShowPassWord, setIsShowPassWord] = useState(false);
+  
   return (
-    <div className="container mx-auto mt-20 flex-row justify-center items-center rounded-xl px-16 pb-16 shadow-2xl w-[60%] bg-white border-gray-200 border-[1px]">
+    <div className="container  mx-auto mb-[6.5rem] mt-24 flex-row justify-center items-center rounded-xl px-12 pb-14 shadow-2xl w-[60%] bg-white border-gray-200 border-[1px]">
       <div className="login-form w-[45%] bg-white rounded-xl inline-block">
-        <div className="text-center text-[32px] pt-4">Đăng nhập</div>
+        <div className="text-center text-3xl pt-4">Đăng nhập</div>
         <div className="form-container p-5 border-gray-400">
           <form action="" onSubmit={() => {alert(`submit login account ${email} with passwd ${password}`)}}>
             <div className="input-field mt-5">
@@ -18,13 +19,14 @@ function LoginPage() {
                 E-mail
               </label>
               <input
-                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:outline-none focus:ring-blue-600 focus:border-blue-700 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 type="email"
                 name="email"
                 id="email"
                 placeholder="example@ctu.edu.vn"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
+                required
               />
             </div>
             <div className="input-field mt-5 relative">
@@ -35,27 +37,27 @@ function LoginPage() {
                 Mật khẩu
               </label>
               <input
-                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:outline-none focus:ring-blue-600 focus:border-blue-700 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 type={isShowPassWord ? "text" : "password"}
                 name="password"
-                id=""
-                placeholder="Mật khẩu"
+                id="password"
+                placeholder={isShowPassWord ? "Mật khẩu" : "•••••••••"}
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
+                required
               />
-              <p
+              <i
               onClick={() => setIsShowPassWord(!isShowPassWord)}
               className="block cursor-pointer bg-white px-1 absolute top-10 right-2">
-              <box-icon
-                
+              <box-icon 
                 title={isShowPassWord ? "Ẩn mật khẩu" : "Hiện mật khẩu"}
                 name={isShowPassWord ? "hide" : "show"}
               ></box-icon>
-              </p>
+              </i>
             </div>
             <button
               type="submit"
-              className="w-full mt-8 text-white bg-second-blue hover:bg-third-blue focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-3 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+              className="w-full mt-8 text-white bg-second-blue hover:bg-third-blue focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-3 text-center dark:bg-blue-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
             >
               Đăng nhập
             </button>
