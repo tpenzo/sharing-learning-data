313bb59e955246b226ca92520637728e3f246486
vinhho3010 <anhvinh30102001@gmail.com>
2023-02-24 14:34:23 +0700

    comment test route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,30 +11,25 @@ import CoursePage from './pages/CoursePage';
 import ManageCourse from './pages/ManageCourse';
 import ManageAccount from './pages/ManageAccount';
 import ManageCourses from './pages/ManageCourses';
-import PageNotFound from './pages/PageNotFound';
-import RequireAuth from './requireAuth';
 
 function App() {
     return (
         <>
             <Routes>
+                <Route exact path="/" element={<HomePage />} />
+                <Route path="/profile" element={<ProfilePage />} />
                 <Route path="/login" element={<LoginPage />} />
-                <Route element={<RequireAuth />}>
-                    <Route exact path="/" element={<HomePage />} />
-                    <Route path="/profile" element={<ProfilePage />} />
 
-                    {/* Courses. Role: student, teacher*/}
-                    <Route path="/courses/:idCourse" element={<CoursePage />} />
-                    <Route path="/courses/:idCourse/manage" element={<ManageCourse />} />
+                {/* Courses. Role: student, teacher*/}
+                <Route path="/courses/:idCourse" element={<CoursePage />} />
+                <Route path="/courses/:idCourse/manage" element={<ManageCourse />} />
 
-                    {/* Role: admin */}
-                    <Route path="/admin/manage" element={<ManageAccount />} />
+                {/* Role: admin */}
+                <Route path="/admin/manage" element={<ManageAccount />} />
 
-                    {/* Role: ministry */}
-                    <Route path="/giaovu/ministry" element={<ManageCourses />} />
+                {/* Role: giaovu */}
+                <Route path="/giaovu/manage" element={<ManageCourses />} />
 
-                </Route>
-                <Route path='*' element={<PageNotFound />} />
             </Routes>
         </>
     )
