549a4f7058cd632b5b31d7cbbdd230cc498621c7
vinhho3010 <anhvinh30102001@gmail.com>
2023-03-03 20:26:35 +0700

    create 404Page

--- client/src/components/sidepane/PopularCoursesList.jsx
+++ client/src/components/sidepane/PopularCoursesList.jsx
@@ -8,7 +8,7 @@ const popularCourseList = ["CT251", "CT252","CT253", "CT259","CT254", "CT255","C
             <div className="w-full h-11/12 popular-courses grid grid-cols-3 gap-1 overflow-y-auto">
                 {popularCourseList.map((popularCourseId, index)=>{
                     return(
-                        <PopularCourseItem popularCourseId={popularCourseId}/>
+                        <PopularCourseItem popularCourseId={popularCourseId} key={index}/>
                     )
                 })}
             </div>
--- client/src/pages/PageNotFound.jsx
+++ client/src/pages/PageNotFound.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
-
+import { useNavigate } from 'react-router-dom';
+import Pic404 from "/assets/404-error.png"
 export default function PageNotFound() {
+  const navigate = useNavigate();
+  const navigateHomePage = (e)=>{
+    e.preventDefault();
+    navigate("/");
+  }
+
     return (
-        <div className="container h-screen">
-            Page Not Found
-        </div>
-    )
+            <div className="container h-screen bg-inherit">
+              <div className="flex flex-row justify-center items-center">
+              <div className="flex flex-col justify-start items-start mt-16">
+                <p className='text-5xl py-7 text-cyan-400 mb-4 font-extrabold'>Không khả dụng!</p>
+                  <p className='font-extralight text-xl text-gray-800 '>Oops! Có vẻ trang bạn truy cập không khả dụng.<br/> Trở về trang chủ bằng cách chọn vào nút bên dưới.</p>
+                  
+                  <button onClick={navigateHomePage} type="button" class="text-white font-semibold text-lg mt-7 bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 rounded-lg px-10 py-2.5 text-center mr-2 mb-2">Trang chủ</button>
+                </div>
+                <div className="w-3/5 h-auto">
+                  <img src={Pic404} alt="err-pic" />
+                </div>
+
+              </div>
+            </div>
+      );
 }
