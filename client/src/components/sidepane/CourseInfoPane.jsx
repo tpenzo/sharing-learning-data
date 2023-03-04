8b4bfb9be979ff613f28606a31a52a6e6a9411bf
vinhho3010 <anhvinh30102001@gmail.com>
2023-03-04 10:25:27 +0700

    add modalEditor into coursePage

--- client/src/components/sidepane/CourseInfoPane.jsx
+++ client/src/components/sidepane/CourseInfoPane.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import { useDisclosure } from "@chakra-ui/react";
 import ListOfUser from "./ListOfUser";
 import TeacherInfo from "./teacherInfo";
+import ModalInstance from "../modal/ModalInstance";
+import CreatePost from "../form/CreatePost";
 function InfoPane() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
+    <>
     <div className="absolute flex-col flex h-full max-h-full w-full">
       <div className="w-full h-1/6 bg-inherit scale-90 2xl:scale-100">
-      <div className="flex flex-row items-center justify-center mx-auto bg-third-blue w-[80%] mt-10 2xl:mt-14 p-3 text-center text-white rounded-xl shadow-lg hover:bg-fourth-blue hover:shadow-lg cursor-pointer">
+      <div
+      onClick={onOpen}
+      className="flex flex-row items-center justify-center mx-auto bg-third-blue w-[80%] mt-10 2xl:mt-14 p-3 text-center text-white rounded-xl shadow-lg hover:bg-fourth-blue hover:shadow-lg cursor-pointer">
           <span className="block px-1 font-semibold">
           <box-icon size="md" color="white" name="plus"></box-icon>
           </span>
@@ -23,6 +31,13 @@ function InfoPane() {
         <ListOfUser />
       </div>
     </div>
+    <ModalInstance
+    isOpen={isOpen}
+    onClose={onClose}
+    modalBody={<CreatePost onClose={onClose} />}
+    modalName={"Tạo bài viết"}
+  />
+    </>
   );
 }
 
--- client/src/components/sidepane/InfoPane.jsx
+++ client/src/components/sidepane/InfoPane.jsx
@@ -12,14 +12,14 @@ function InfoPane() {
       <div className="flex-col flex h-full overflow-y-auto">
         {/* post content */}
         <div className="w-full h-[13%] 2xl:h-1/5 bg-inherit scale-[85%] 2xl:scale-100">
-          <div className="flex flex-row items-center justify-center mx-auto bg-third-blue w-[80%] mt-10 2xl:mt-14 p-3 text-center text-white rounded-xl shadow-lg hover:bg-fourth-blue hover:shadow-lg cursor-pointer">
+          <div
+            onClick={onOpen}
+            className="flex flex-row items-center justify-center mx-auto bg-third-blue w-[80%] mt-10 2xl:mt-14 p-3 text-center text-white rounded-xl shadow-lg hover:bg-fourth-blue hover:shadow-lg cursor-pointer"
+          >
             <span className="block px-1 font-semibold">
               <box-icon size="md" color="white" name="plus"></box-icon>
             </span>
-            <span
-              onClick={onOpen}
-              className="block font-semibold text-base pb-1 ml-1 mr-3"
-            >
+            <span className="block font-semibold text-base pb-1 ml-1 mr-3">
               Đăng bài
             </span>
           </div>
