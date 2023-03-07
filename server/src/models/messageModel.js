50a218c138817856ee6654869732e56bbc9cbd02
tpEnzo <87224628+EnzoMrPhuc@users.noreply.github.com>
2023-03-07 09:08:50 +0700

    Message route and ctrl

--- server/src/models/messageModel.js
+++ server/src/models/messageModel.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const messageSchema = new Schema({
   sender: {type: Schema.Types.ObjectId, ref: 'user'},
   content: {type: String},
-  chat: {type: Schema.Types.ObjectId, ref: 'chat'}
+  chatId: {type: Schema.Types.ObjectId, ref: 'chat'}
 },{timestamps: true})
 
 const message = mongoose.model('message', messageSchema);
